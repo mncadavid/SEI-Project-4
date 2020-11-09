0e1a92a1cea73a47fd0c2b9af85f1f624cc9dcdc
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-09 11:35:51 -0600

    Basic Splash Page Created

--- project4/src/App.js
+++ project4/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App body">
       <Header />
-      <SplashPage />
+      {/* <SplashPage /> */}
       <Footer />
     </div>
   );
--- project4/src/Components/SplashPage.jsx
+++ project4/src/Components/SplashPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import {Button} from 'antd';
 
 function SplashPage(){
     return(
-        <div>
-
+        <div className="splash-page">
+            <h1>Food Folio</h1>
+            <img className="splash-image" src="./food-clipart.png" alt="Logo"/>
+            <div>
+                <Button type="primary" size="large" className="auth-button">Log In</Button>
+                <Button type="primary" className="auth-button">Sign Up</Button>
+            </div>
         </div>
     )
 }
