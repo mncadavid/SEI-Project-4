a8f2f1403e434edb866eac504766ccd2a6bf8d5a
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-09 15:02:02 -0600

    Moved jsx files into categorized folders

--- project4/src/App.js
+++ project4/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import SplashPage from './Components/SplashPage';
-import FoodModal from './Components/FoodModal';
-import ListPage from './Components/ListPage';
-import BrowsePage from './Components/BrowsePage';
+import FoodModal from './Components/ExposureModal/FoodModal';
+import ListPage from './Components/ListPage/ListPage';
+import BrowsePage from './Components/BrowsePage/BrowsePage';
 import AccountPage from './Components/AccountPage';
 
 function App() {
