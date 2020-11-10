df636dd0b8d2f023fc19c4ba0b493474562dbf17
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-10 12:40:27 -0600

    set up verify token in server.js

--- project-4-api/server.js
+++ project-4-api/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+
 const routes = require('./routes');
 const app = express();
+
+const verifyToken = (req,res,next) => {
+    let token = req.cookies.jwt;
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if(err || !decodedUser){
+            if(err.name == "JsonWebTokenError"){
+                return res.status(400).send(`Error: ${err}`);
+            }
+            return res.send(err);
+        }
+        req.user = decodedUser;
+        next();
+    });
+}
 
 app.use('/users', routes.users);
 app.use('/lists', routes.lists);
