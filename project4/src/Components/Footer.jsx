0a81207e2f71011f5c538e7617b7b7a0853ded03
Marissa Cadavid <cadavidbernsmarissa@johndeere.com>
2020-11-17 16:36:37 -0600

    Updated links in footer to open new tab

--- project4/src/Components/Footer.jsx
+++ project4/src/Components/Footer.jsx
@@ -5,8 +5,10 @@ function Footer(){
     return(
         <div className="footer">
             <Link to="/about" style={{textDecoration:'none',color:'black'}}>Why Picky Preventer?</Link>
-            <a href="https://www.linkedin.com/in/marissaberns/">Created by Marissa Cadavid Berns</a>
-            <a href="https://github.com/mncadavid/SEI-Project-4/issues">Report an Issue</a>
+            <a href="https://www.linkedin.com/in/marissaberns/"
+            target="_blank" rel="noreferrer">Created by Marissa Cadavid Berns</a>
+            <a href="https://github.com/mncadavid/SEI-Project-4/issues"
+            target="_blank" rel="noreferrer">Report an Issue</a>
         </div>
     )
 }
